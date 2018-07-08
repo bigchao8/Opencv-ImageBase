6cbe01e2e7f8952e340010ce749b7cccf2287088
“bigchao8 <1196897145@qq.com>
2018-07-09 02:16:04 +0800

    增加文字格式化输出编译

--- bulid.sh
+++ bulid.sh
@@ -1,2 +1,5 @@
 cd main
 g++ *.cpp -o img_preproc -std=c++11 -lpthread `pkg-config --cflags --libs opencv`
+cd .. 
+cd box-format
+g++ box-format2.cpp -o box-format -std=c++11 -lpthread `pkg-config --cflags --libs opencv`
